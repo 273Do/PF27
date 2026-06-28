c4e096cf70580d50082faf1a04ab5e18732c24dd
273* <keita.y310@icloud.com>
2026-06-28 19:58:29 +0900

    chore: add react settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
+import react from "@astrojs/react";
 import { defineConfig, fontProviders } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
   },
