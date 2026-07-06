72ebc8767b12c625a17709e6fb618a5bfd90624c
273* <keita.y310@icloud.com>
2026-07-07 00:32:37 +0900

    feat: add dither default property

--- src/constants/3d.ts
+++ src/constants/3d.ts
@@ -1,3 +1,5 @@
+import * as THREE from "three";
+
 export const CAGE = {
   FLOOR_X: 5.35,
   FLOOR_Z: 2.85,
@@ -8,3 +10,15 @@ export const CAGE = {
     return this.FLOOR_Y + this.WALL_H;
   },
 } as const;
+
+export const DITHER_DEFAULT_OPTIONS = {
+  time: 0,
+  resolution: new THREE.Vector2(1, 1),
+  gridSize: 4.0,
+  luminanceMethod: 0,
+  invertColor: false,
+  pixelSizeRatio: 5,
+  grayscaleOnly: true,
+  foregroundColor: "#1B1A18",
+  backgroundColor: "#F6F6F5",
+};
