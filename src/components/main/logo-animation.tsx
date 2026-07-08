9f68661ce82e967f8a172e53258810bd1f4c0567
273* <keita.y310@icloud.com>
2026-07-08 22:45:47 +0900

    feat: add gl settings

--- src/components/main/logo-animation.tsx
+++ src/components/main/logo-animation.tsx
@@ -3,6 +3,7 @@ import { Suspense } from "react";
 import { Environment, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Physics } from "@react-three/rapier";
+import * as THREE from "three";
 
 import background from "@/assets/imgs/empty_warehouse_01_1k.hdr";
 
@@ -15,7 +16,13 @@ const DEBUG_MODE = false;
 export function LogoAnimation() {
   return (
     <div className="my-4 h-72 w-full border-[0.1px] border-dashed">
-      <Canvas camera={{ position: [0, 5, 0], up: [0, 0, -1], fov: 50 }}>
+      <Canvas
+        camera={{ position: [0, 5, 0], up: [0, 0, -1], fov: 50 }}
+        gl={{
+          antialias: false,
+          powerPreference: "high-performance",
+        }}
+      >
         {DEBUG_MODE && (
           <>
             <gridHelper />
