e8375c47a36b34a0532a958702ea5c31b5443911
273* <keita.y310@icloud.com>
2026-07-11 01:21:50 +0900

    feat: add seo settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,26 @@
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 import { defineConfig, fontProviders } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
+import { loadEnv } from "vite-plus";
+
+const { LOCAL_URL } = loadEnv(process.env.NODE_ENV ?? "production", process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: process.env.URL || LOCAL_URL,
+  integrations: [
+    sitemap({
+      filter: (page) => !/\/(404|500)(\/|$)/.test(new URL(page).pathname),
+    }),
+    react(),
+  ],
   vite: {
+    server: {
+      // TODO: 検証時のみ
+      allowedHosts: true,
+    },
     plugins: [
       tailwindcss(),
       {
---
+++ src/constants/metadata.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = "273* Portfolio";
+export const SITE_DESCRIPTION = "Hello. I love CREATIVE.";
