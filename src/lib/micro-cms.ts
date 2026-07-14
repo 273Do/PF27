f468676407ef73c31f14af599d4e9fd26c44e326
273* <keita.y310@icloud.com>
2026-07-14 23:45:33 +0900

    fix: microcms settings

--- src/lib/micro-cms.ts
+++ src/lib/micro-cms.ts
@@ -1,12 +1,6 @@
 import { createClient } from "microcms-js-sdk";
-import { loadEnv } from "vite-plus";
-
-const env = loadEnv("", process.cwd(), "");
-
-const SERVICE_DOMAIN = env.MICROCMS_SERVICE_DOMAIN;
-const API_KEY = env.MICROCMS_API_KEY;
 
 export const client = createClient({
-  serviceDomain: SERVICE_DOMAIN,
-  apiKey: API_KEY,
+  serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: import.meta.env.MICROCMS_API_KEY,
 });
