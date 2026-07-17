614e19d642744e16209e9a87a2ec1614f293f777
273* <keita.y310@icloud.com>
2026-07-17 19:54:52 +0900

    feat: add curl profile

--- src/constants/curl.ts
+++ src/constants/curl.ts
@@ -1,3 +1,7 @@
+import { getProfile } from "@/lib/fetch-contents";
+
+import { SNS_LINKS } from "./links";
+
 const C = {
   lightGray: "\x1b[37m",
   gray: "\x1b[90m",
@@ -9,6 +13,11 @@ const C = {
   reset: "\x1b[0m",
 };
 
+const linkText = (url: string, text: string) =>
+  `\x1b]8;;${url}\x07${C.underline}${text}\x1b]8;;\x07${C.reset}`;
+
+const { profile } = await getProfile();
+
 export const SELF_INTRO = `
   ${C.lightGray}${C.bold}____ _____ _____        ${C.reset}${C.gray}${C.bold}__    __           _${C.reset}
  ${C.lightGray}${C.bold}|___ |___  |___ /__/\\__ ${C.reset}${C.gray}${C.bold}/ / /\\ \\ \\___  _ __| | _____${C.reset}
@@ -17,5 +26,8 @@ export const SELF_INTRO = `
  ${C.lightGray}${C.bold}|_____/_/  |____/  \\/     ${C.reset}${C.gray}${C.bold}\\/  \\/ \\___/|_|  |_|\\_|___/${C.reset}
 
 
-${C.bgWhite}${C.black}273* (tuna-sand)${C.reset}
+${C.bgWhite}${C.black}${profile.name}${C.reset}
+${C.gray}${profile.bio}${C.reset}
+
+${SNS_LINKS.map((link) => `${C.lightGray}${link.title}${C.reset}${C.gray} : ${linkText(link.url, link.name)}${C.reset}`).join("\n")}
 `;
